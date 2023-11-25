664a97395068cc4e75f5913e5abced6b71093c2e
이희성 <ihuiseong@ihuiseong-ui-noteubug.local>
2023-11-25 13:26:40 +0900

    ADD :: 데이터 전처리

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from flask import Flask, render_template, jsonify
 
-# Load the CSV file
-file_path = './data/Annual_Surface_Temperature_Change.csv'
-surface_temp_data = pd.read_csv(file_path)
+# 데이터 로드
+climate_data = pd.read_csv('./data/Annual_Surface_Temperature_Change.csv')
 
-surface_temp_data.head()
+# 불필요한 열 제거
+columns_to_drop = ['ObjectId', 'ISO2', 'ISO3', 'CTS_Code', 'Source', 'CTS_Name', 'CTS_Full_Descriptor']
+data = climate_data.drop(columns=columns_to_drop)
